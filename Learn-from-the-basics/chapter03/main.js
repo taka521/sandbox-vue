3e1734ff322f5baf58a07e0e0139a0298803dcaa
taka <521taka.m@gmail.com>
2018-07-21 21:44:58 +0900

    ちゃぷたー３、おわり

--- Learn-from-the-basics/chapter03/main.js
+++ Learn-from-the-basics/chapter03/main.js
@@ -2,9 +2,38 @@ var app = new Vue({
     el: "#app",
     data: {
         count: 0,
-        show: false
+        show: false,
+        message: 'Hello!',
+        val: true,
+        val2: 'yes',
+        val3: [],
+        val4: [],
+        val5: '',
+        val6: [],
+        preview: '',
+        price: 100,
+        scrollY: 0,
+        timer: null
+    },
+    created: function() {
+        // ハンドラを登録
+        window.addEventListener('scrolle', this.handleScroll)
+    },
+    beforeDestory: function() {
+        // ハンドラを解除
+        window.removeEventListener('scrolle', this.handleScroll)
     },
     methods: {
+        handleScroll: function() {
+            console.log(this.scrollY)
+            if (this.timer === null) {
+                this.timer = setTimeout(function(){
+                    this.scrollY = window.scrollY
+                    clearTimeout(this.timer)
+                    this.timer = null
+                }.bind(this), 200)
+            }
+        },
         clickHandler: function() {
             alert("クリックされました！")
         },
@@ -13,6 +42,12 @@ var app = new Vue({
         },
         handle: function(comment) {
             console.log(comment)
+        },
+        handleChange: function(event) {
+            var file = event.target.files[0];
+            if (file && file.type.match(/^image\/(png|jpeg)$/)) {
+                this.preview = window.URL.createObjectURL(file)
+            }
         }
     }
 })
