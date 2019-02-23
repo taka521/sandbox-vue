70d665502a749c0fe033e3e93ebfc748376c9574
taka <521taka.m@gmail.com>
2019-02-24 00:55:12 +0900

    [nuxt-jest-example]vuexストアを利用しているケースの処理を作成

---
+++ nuxt-jest-example/app/store/index.js
@@ -0,0 +1,23 @@
+const state = () => ({
+  count: 0
+});
+
+const getters = {
+  count: (state) => state.count
+};
+
+const mutations = {
+  increment(state) {
+    state.count++;
+  }
+};
+
+const actions = {
+  increment({ commit }) {
+    commit('increment');
+  }
+};
+
+export default {
+  state, getters, mutations, actions
+};
