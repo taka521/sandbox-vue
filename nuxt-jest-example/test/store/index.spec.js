bde1384852457d807520125b82cd7b996ad68023
taka <521taka.m@gmail.com>
2019-02-24 16:58:10 +0900

    jestを用いた単体テストの作成および、テスト実行に必要なパッケージの追加

---
+++ nuxt-jest-example/test/store/index.spec.js
@@ -0,0 +1,33 @@
+import Vuex               from "vuex";
+import index              from '../../app/store' ;
+import { createLocalVue } from '@vue/test-utils';
+import cloneDeep          from 'lodash.clonedeep';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('store/index.js', () => {
+
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store(cloneDeep(index));
+  });
+
+  describe('mutations', () => {
+    test('increment ミューテーションがコミットされると、count ステートの値が +1 される', () => {
+      expect(store.getters['count']).toBe(0);
+      store.commit('increment');
+      expect(store.getters['count']).toBe(1);
+    });
+  });
+
+  describe('actions', () => {
+    test('increment アクションを dispach するたびに、increment ミューテションがコミットされる', () => {
+      expect(store.getters['count']).toBe(0);
+      store.dispatch('increment');
+      expect(store.getters['count']).toBe(1);
+    });
+  });
+
+});
